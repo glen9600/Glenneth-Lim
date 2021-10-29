b26a9f7324decd3e6aa16083ebc0d2056774266c
glen9600 <62791465+glen9600@users.noreply.github.com>
2021-10-29 16:07:29 +0800

    Update Price_Calculator.js

--- Price_Calculator.js
+++ Price_Calculator.js
@@ -1,11 +1,12 @@
 var Number_Of_LTC = document.getElementById(NumberOfLTC).replace(/\D/g,''); //Extract from HTML and filter to remove non-numeric characters
 var Number_Of_CRO = document.getElementById(NumberOfCRO).replace(/\D/g,'');
 
-function CalculateTotalProfit(Number_Of_CRO,Number_Of_LTC)
+function CalculateTotalProfit(Total_USD_Profit)
 {
   var LTC_Profit = Number_Of_LTC * 190;
   var CRO_Profit = Number_Of_CRO * 0.20;
   let Total_USD_Profit =  LTC_Profit + CRO_Profit;
- return Total_USD_Profit;
+ console.log(Total_USD_Profit);
   }
-document.getElementById("Total_Profit").innerHTML = "Total Profit in USD is:" + Total_USD_Profit;
+
+function CalculateTotalProfit(Total_USD_Profit)
